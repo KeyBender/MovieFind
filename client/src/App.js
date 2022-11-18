57542ea6344ee2f8ef8aaffe1c6f504525fe910c
KeyBender <crooks.cameron33@gmail.com>
2022-11-18 15:37:24 -0800

    I have no clue what i did

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
-import "./App.css";
-import SignInSide from "./views/SignIn";
-import SignUpSide from "./views/SignUp";
-import Index from "./views/Index";
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="bg-primary">
       <Routes>
-        <Route path="/" element={<Index/>}/>
-        <Route path="/signin" element={<SignInSide/>}/>
-        <Route path="/signup" element={<SignUpSide/>}/>
       </Routes>
     </div>
   );
