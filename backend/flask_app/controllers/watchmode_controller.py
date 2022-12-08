e3bb7309751941520078fd6f6d4615bf610d1296
KeyBender <crooks.cameron33@gmail.com>
2022-12-08 09:54:05 -0700

    hid api key in .gitignore file

--- backend/flask_app/controllers/watchmode_controller.py
+++ backend/flask_app/controllers/watchmode_controller.py
@@ -2,12 +2,20 @@ from flask import jsonify, request
 import urllib.request
 import json
 from flask_app import app
+from dotenv import dotenv_values
 
-YOUR_API_KEY = "yRaMJT9UguDGDaaHn2ygddFzTsHD6H1UkoQHVF1s"
+api_key = dotenv_values('.env')['YOUR_API_KEY']
 
 @app.route("/api/watchmode/init")
 def watchmode_call():
-    with urllib.request.urlopen(f'https://api.watchmode.com/v1/list-titles/?apiKey={YOUR_API_KEY}') as url:
+    with urllib.request.urlopen(f'https://api.watchmode.com/v1/list-titles/?apiKey={api_key}') as url:
+        data = json.loads(url.read().decode())
+        print(data)
+        return jsonify(data), 200
+
+@app.route("/api/watchmode/search/<searchValue>")
+def watchmode_search(searchValue):
+    with urllib.request.urlopen(f'https://api.watchmode.com/v1/autocomplete-search/?apiKey={api_key}&search_value={searchValue}&search_type=1') as url:
         data = json.loads(url.read().decode())
         print(data)
         return jsonify(data), 200
--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ function App(props) {
               .catch(err=> console.log(err))
   }
   
-
   useEffect(()=>{
     setFilms(backendFilmPopulate())
   },[])
